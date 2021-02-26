a0ce7d00f0bd8aa5564d654fd1c1723a3bd67ef9
Thais de Lima Santos <thais_tlsan@hotmail.com>
2021-02-26 02:03:15 -0300

    Desenvolvendo

--- login.py
+++ login.py
@@ -7,7 +7,21 @@ from tkinter import messagebox
 janela = Tk()
 janela.title('TLS Consultoria em TI - Painel de Acesso')
 janela.geometry('600x300')
-janela.configure('white')
-# janela.resizable(width = False, twight = False) ---> ñ ser responível
+janela.configure(background='white')
+janela.resizable(width=False, height=False)
+
+# carregar as imagens
+logo = PhotoImage(file='icons/logo.png')
+
+# criação dos WIDGETS
+LeftFrame = Frame(janela, width=200, height=300, bg='#BDE9D4', relief='raise')
+LeftFrame.pack(side=LEFT)
+
+RightFrame = Frame(janela, width=395, height=300, bg='#BDE9D4', relief='raise')
+RightFrame.pack(side=RIGHT)
+
+LogoLabel = Label(LeftFrame, image=logo, bg='white')
+LogoLabel.place(x=25, y=80)
+
 
 janela.mainloop()
