35dad874aa9c27213f4bf4532ddc2a8172899304
Thais de Lima Santos <thais_tlsan@hotmail.com>
2021-02-27 00:41:25 -0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Sistema-de-Login
- Projeto para Criação de Tela de Login - Python e SQL
+
+# Em Construção
+
+Projeto para Criação de Tela de Login - Python com integração em SQL Server
+
+Criação de sistema de login, com inserção de logo e favicon;
+
+Criação interface gráfica de usuário (GUI) com Tkinter;
+
+Criação de banco de dados;
+
+Integração do banco de dados com seus distemas de forma organizada;
--- login.py
+++ login.py
@@ -3,25 +3,28 @@
 from tkinter import *
 from tkinter import messagebox
 
-# criando a janela/ atribuindo uma janela a variável
+# --------- criando a janela/ atribuindo uma janela a uma variável
 janela = Tk()
 janela.title('TLS Consultoria em TI - Painel de Acesso')
 janela.geometry('600x300')
 janela.configure(background='white')
 janela.resizable(width=False, height=False)
 
-# carregar as imagens
+# --------- carregar as imagens
 logo = PhotoImage(file='icons/logo.png')
 
-# criação dos WIDGETS
+# --------- criação dos WIDGETS
+# --------- lado esquerdo
 LeftFrame = Frame(janela, width=200, height=300, bg='#BDE9D4', relief='raise')
 LeftFrame.pack(side=LEFT)
 
+# --------- lado direito com diminuição de pixel para mostrar uma divisão 
 RightFrame = Frame(janela, width=395, height=300, bg='#BDE9D4', relief='raise')
 RightFrame.pack(side=RIGHT)
 
-LogoLabel = Label(LeftFrame, image=logo, bg='white')
-LogoLabel.place(x=25, y=80)
+
+LogoLabel = Label(LeftFrame, image=logo, bg='#BDE9D4')
+LogoLabel.place(x=0, y=50)
 
 
 janela.mainloop()
